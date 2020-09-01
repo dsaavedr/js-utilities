742ecc6b8dbe1382f60882f12b7331895cac65a3
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-09-01 16:28:59 -0500

    Added vectors to particles for physics simulation

--- particle.js
+++ particle.js
@@ -1,7 +1,7 @@
 class Particle {
     constructor(pos, vel, c = 'white', r = 4) {
         this.pos = pos;
-        this.acc = [0, 0];
+        this.acc = new Vector(0, 0);
         this.vel = vel;
         this.r = r;
         this.children = [];
@@ -14,24 +14,22 @@ class Particle {
     }
 
     update() {
-        this.vel[0] += this.acc[0];
-        this.vel[1] += this.acc[1];
-        this.pos[0] += this.vel[0];
-        this.pos[1] += this.vel[1];
-        this.acc = 0;
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc = new Vector(0, 0);
     }
 
     show() {
-        point(this.pos[0], this.pos[1], this.c, this.r);
+        point(this.pos.x, this.pos.y, this.c, this.r);
     }
 
     explode(n) {
         for (var i = 0; i < n; i++) {
-            var temp = this.pos.slice();
-            this.children.push(new Particle(temp, [
-                random(-4, 4),
-                random(-5, 2)
-            ], this.c, 2));
+            var temp = this.pos.copy();
+            var vel = Vector.random().mult(random(-5, 5));
+            this.children.push(new Particle(temp,
+                vel,
+                this.c, 2));
         }
     }
 }
