d06bfdfff228440eba3aaca5c24de0a080f9e43c
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-09-30 10:54:27 -0500

    Rewrote the log function to reference console.log directly instead of just passing arguments

--- auxFunctions.js
+++ auxFunctions.js
@@ -1,6 +1,4 @@
-function log(s) {
-    console.log(s);
-}
+const log = console.log;
 
 function scale(num, in_min, in_max, out_min, out_max) {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
