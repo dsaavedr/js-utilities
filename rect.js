64ce721b793e9b4b7c7429c4673a181f6269ec21
Daniel Saavedra <danielsaavedram@hotmail.com>
2022-04-18 19:15:36 -0500

    Added update function and separate fill and stroke colors

--- rect.js
+++ rect.js
@@ -1,10 +1,18 @@
 class Rect {
-    constructor({ pos, size, stroke = false, fill = true, c = "white" }) {
+    constructor({
+        pos,
+        size,
+        stroke = false,
+        fill = true,
+        fillColor = "white",
+        strokeColor = "black"
+    }) {
         this.pos = pos;
-        this.size = size;
+        this.size = typeof size === "number" ? new Vector(size, size) : size;
         this.stroke = stroke;
         this.fill = fill;
-        this.c = c;
+        this.fillColor = fillColor;
+        this.strokeColor = strokeColor;
     }
 
     draw() {
@@ -12,16 +20,34 @@ class Rect {
         ctx.rect(this.pos.x, this.pos.y, this.size.x, this.size.y);
         if (this.fill) {
             ctx.save();
-            ctx.fillStyle = this.c;
+            ctx.fillStyle = this.fillColor;
             ctx.fill();
             ctx.restore();
         }
         if (this.stroke) {
             ctx.save();
-            ctx.strokeStyle = this.c;
+            ctx.strokeStyle = this.strokeColor;
             ctx.stroke();
             ctx.restore();
         }
         ctx.closePath();
+    }
+
+    update({
+        pos = this.pos,
+        size = this.size,
+        stroke = this.stroke,
+        fill = this.fill,
+        fillColor = this.fillColor,
+        strokeColor = this.strokeColor
+    }) {
+        this.pos = pos;
+        this.size = size;
+        this.stroke = stroke;
+        this.fill = fill;
+        this.fillColor = fillColor;
+        this.strokeColor = strokeColor;
+
+        return this;
     }
 }
